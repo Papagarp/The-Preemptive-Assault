2f49189f2fa51d2be9d209a13c51018a34ef5ec3
Isaac <Isaacgarpendal97@gmail.com>
2019-09-29 17:56:29 +1000

    Some cool Ai things

---
+++ Assets/Scripts/Isaac's Ai/AiDetection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class AiDetection : MonoBehaviour
+{
+    public AiMovement aiMovementScript;
+
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask obstacleMask;
+    public LayerMask targetMask;
+
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2f);
+
+        aiMovementScript = GetComponent("AiMovement") as AiMovement;
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true){
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float disToTarget = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.position, dirToTarget,disToTarget, obstacleMask))
+                {
+                    aiMovementScript.foundPlayer = true;
+                }
+            }
+        }
+    }
+
+    public Vector3 directionFromAngle(float angleInDegrees, bool angleIsGlobal) {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
---
+++ Assets/Scripts/Isaac's Ai/AiMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using System.Linq;
+
+public class AiMovement : MonoBehaviour
+{
+    //public PlayerMovement playerMovementScript;
+
+    public GameObject[] targets;
+
+    GameObject player;
+    
+    NavMeshAgent nav;
+
+    public bool foundPlayer = false;
+
+    public float killDistance = 1.5f;
+
+    int i = 0;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        //playerMovementScript = GetComponent("PlayerMovement") as PlayerMovement;
+
+        nav.SetDestination(targets[i].transform.position);
+    }
+
+    void Update()
+    {
+        float distanceToTarget = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(targets[i].transform.position.x, 0, targets[i].transform.position.z));
+
+        float distanceToPlayer = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(player.transform.position.x, 0, player.transform.position.z));
+
+        if (i == (targets.Length-1) && distanceToTarget <= 0.1f)
+        {
+            i = 0;
+            nav.SetDestination(targets[i].transform.position);
+        }
+
+        else if (distanceToTarget <= 0.1f)
+        {
+            i++;
+            nav.SetDestination(targets[i].transform.position);
+        }
+
+        if (foundPlayer == true)
+        {
+            nav.SetDestination(player.transform.position);
+        }
+
+        if (distanceToPlayer <= killDistance)
+        {
+            nav.SetDestination(transform.position);
+            //playerMovementScript.playerCaught = true;
+        }
+    }
+}
