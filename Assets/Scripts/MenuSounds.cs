f9839705041bafa69458ba90850d4772e948d236
JesseNellis <40745615+JesseNellis@users.noreply.github.com>
2020-03-24 17:48:30 +1100

    hg

--- Assets/Scripts/MenuSounds.cs
+++ Assets/Scripts/MenuSounds.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuSounds : MonoBehaviour
 {
@@ -8,13 +9,32 @@ public class MenuSounds : MonoBehaviour
     public AudioClip hoverSFX;
     public AudioClip clickSFX;
 
+    public GameObject pauseFirst;
+
     public void HoverSound()
     {
+        
         menuSFX.PlayOneShot(hoverSFX);
+
+
     }
 
     public void ClickSound()
     {
         menuSFX.PlayOneShot(clickSFX);
+    }
+
+    public void MenuFirst()
+    {
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        //Set a new selected object
+        EventSystem.current.SetSelectedGameObject(pauseFirst);
+    }
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
