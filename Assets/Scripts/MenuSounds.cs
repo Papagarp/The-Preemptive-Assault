2b50173ec85a92e4ac14238ec5070fe3c7ff5a1f
JesseNellis <40745615+JesseNellis@users.noreply.github.com>
2020-03-24 13:48:38 +1100

    momomom

---
+++ Assets/Scripts/MenuSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSounds : MonoBehaviour
+{
+    public AudioSource menuSFX;
+    public AudioClip hoverSFX;
+    public AudioClip clickSFX;
+
+    public void HoverSound()
+    {
+        menuSFX.PlayOneShot(hoverSFX);
+    }
+
+    public void ClickSound()
+    {
+        menuSFX.PlayOneShot(clickSFX);
+    }
+}
