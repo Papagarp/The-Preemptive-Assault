527ab21d753412227f902a89de25e898cedf687c
JesseNellis <40745615+JesseNellis@users.noreply.github.com>
2020-03-28 20:09:57 +1100

    only the most epic of intros over here, sicknasty

---
+++ Assets/Scripts/Jesse/Intro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    public Animator booBoo;
+   
+        
+    void Start()
+    {
+        StartCoroutine(Splash());
+    }
+
+    public IEnumerator Splash()
+    {
+        yield return new WaitForSeconds(5);
+
+        booBoo.SetTrigger("Fade Out");
+
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/Jesse/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator mainMenu;
+
+    public void Start()
+    {
+        StartCoroutine(GamerIntro());
+    }
+
+    public IEnumerator GamerIntro()
+    {
+        yield return new WaitForSeconds(2);
+
+        mainMenu.SetTrigger("Title Done");
+    }
+
+    public void Gamertime()
+    {
+        StartCoroutine(Extremo());
+    }
+
+    public IEnumerator Extremo()
+    {
+        mainMenu.SetTrigger("Choice Made");
+
+        yield return new WaitForSeconds(2);
+
+        mainMenu.SetTrigger("Option Gone");
+
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene("JesseTest");
+    }
+
+}
