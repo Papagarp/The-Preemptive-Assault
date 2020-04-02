87210b73a5d79d9f520223c898c6b26135550783
JesseNellis <40745615+JesseNellis@users.noreply.github.com>
2020-04-03 01:53:43 +1100

    Finished Checkpoint system : ^ )

---
+++ Assets/Scripts/Jesse/Checkpoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gm.lastCheckPoint = transform.position;
+            Debug.Log("epep");
+        }
+   
+    }
+}
---
+++ Assets/Scripts/Jesse/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    private static GameMaster instance;
+    public Vector3 lastCheckPoint;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
