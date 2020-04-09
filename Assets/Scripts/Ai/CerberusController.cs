ee792c27b6820ad36ea991e7b208cde4e1aa60d7
Isaac <Isaacgarpendal97@gmail.com>
2020-04-09 21:19:03 +1000

    Update CerberusController.cs

--- Assets/Scripts/Ai/CerberusController.cs
+++ Assets/Scripts/Ai/CerberusController.cs
@@ -5,25 +5,143 @@ using UnityEngine.AI;
 
 public class CerberusController : MonoBehaviour
 {
+    //animation
+    Coroutine normalBiteCo;
+    Coroutine tripleBiteCo;
+    Coroutine fireEyeBeamCo;
+    Animator cerberusAnim;
+
+    //nav mesh
+    NavMeshAgent nav;
+
+    //player
     GameObject player;
-
+    CharController playerScript;
     Vector3 playerLocation;
-
     float distanceToPlayer;
 
+    //enviroment
+    float roomSize;
+    Vector3 startingPosition;
+    Quaternion startingRotation;
+
+    //attacks
+    bool isAttacking;
+    float attackTimer = 0.1f;
     float normalBiteDmg = 25.0f;
     float tripleHeadBiteDmg = 50.0f;
     float fireEyeBeamDmg = 10.0f;
 
+    private void Awake()
+    {
+        cerberusAnim = GetComponentInChildren<Animator>();
+
+        nav = GetComponent<NavMeshAgent>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<CharController>();
+    }
+
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        startingPosition = gameObject.transform.position;
+        startingRotation = gameObject.transform.rotation;
     }
 
     private void Update()
     {
         playerLocation = player.transform.position;
 
-        distanceToPlayer = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(playerLocation.x, 0, playerLocation.z));
+        distanceToPlayer = Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), 
+                           new Vector3(playerLocation.x, 0, playerLocation.z));
+
+        if (distanceToPlayer < roomSize && !isAttacking)
+        {
+            attackTimer -= Time.deltaTime;
+            if (attackTimer <= 0)
+            {
+                int i = Random.Range(0, 2);
+
+                if (i == 0)
+                {
+                    NormalBite();
+                }
+                else if (i == 1)
+                {
+                    TripleHeadBite();
+                }
+                else if (i == 2)
+                {
+                    FireEyeBeam();
+                }
+
+                isAttacking = true;
+            }
+        }
+    }
+
+    public void NormalBite()
+    {
+        if (distanceToPlayer < 2.0f)
+        {
+            if (normalBiteCo == null) normalBiteCo = StartCoroutine(NormalBiteAnim());
+        }
+        else
+        {
+            nav.SetDestination(player.transform.position);
+        }
+    }
+
+    IEnumerator NormalBiteAnim()
+    {
+        cerberusAnim.SetTrigger("");
+
+        float clipLength = -1;
+        foreach (AnimationClip c in cerberusAnim.runtimeAnimatorController.animationClips)
+        {
+            if (c.name == "")
+            {
+                clipLength = c.length;
+            }
+        }
+
+        yield return new WaitForSeconds(clipLength);
+
+        isAttacking = false;
+    }
+
+    public void TripleHeadBite()
+    {
+        if (distanceToPlayer < 2.0f)
+        {
+            if (tripleBiteCo == null) tripleBiteCo = StartCoroutine(TripleBiteAnim());
+        }
+        else
+        {
+            nav.SetDestination(player.transform.position);
+        }
+    }
+
+    IEnumerator TripleBiteAnim()
+    {
+        cerberusAnim.SetTrigger("");
+
+        float clipLength = -1;
+        foreach (AnimationClip c in cerberusAnim.runtimeAnimatorController.animationClips)
+        {
+            if (c.name == "")
+            {
+                clipLength = c.length;
+            }
+        }
+
+        yield return new WaitForSeconds(clipLength);
+
+        isAttacking = false;
+    }
+
+    public void FireEyeBeam()
+    {
+
     }
 }
