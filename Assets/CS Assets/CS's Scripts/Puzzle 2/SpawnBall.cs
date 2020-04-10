3d55af7f6d8107297cb0197414eb1e5503d997e0
CShepherdSAE <44623939+CShepherdSAE@users.noreply.github.com>
2020-04-10 15:36:07 +1000

    Puzzle 2 Spawn Works

---
+++ Assets/CS Assets/CS's Scripts/Puzzle 2/SpawnBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBall : MonoBehaviour
+{
+
+    public GameObject ballSpawn;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        Debug.Log("its starting");
+    }
+
+   public void SpawnObject()
+    {
+        Instantiate(ballSpawn, transform.position, transform.rotation);
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
