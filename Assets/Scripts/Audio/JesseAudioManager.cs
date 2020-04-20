b92bf1d02ed089a30c20147ae6fd17f8eadfea1e
JesseNellis <40745615+JesseNellis@users.noreply.github.com>
2020-04-20 17:13:54 +1000

    did some player stuff

---
+++ Assets/Scripts/Audio/JesseAudioManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class JesseAudioManager : MonoBehaviour
+{
+	[Serializable]
+	public class AudioClass
+	{
+		public string name;
+		public AudioSource source;
+	}
+	
+	public AudioClass[] sources;
+
+	public void PlaySound(string Name)
+	{
+		foreach(AudioClass c in sources)
+		{
+			if(c.name == Name)
+			{
+				c.source.Play();
+			}
+		}
+	}
+
+	public void PlaySound(string Name, AudioClip Clip)
+	{
+		foreach(AudioClass c in sources)
+		{
+			if(c.name == Name)
+			{
+				Debug.Log(c.name);
+				c.source.clip = Clip;
+				c.source.Play();
+			}
+		}
+	}
+
+	public void StopSound(string Name)
+	{
+		foreach(AudioClass c in sources)
+		{
+			if(c.name == Name)
+			{
+				c.source.Stop();
+			}
+		}
+	}
+
+	public void PlayOneShot(Vector3 position, AudioClip clip, float blend)
+	{
+		GameObject obj = new GameObject();
+
+		AudioSource source = obj.AddComponent<AudioSource>();
+		source.spatialBlend = blend;
+		source.clip = clip;
+		source.playOnAwake = false;
+
+		obj.transform.position = position;
+
+		source.Play();
+
+		Destroy(obj, clip.length);
+	}
+}
---
+++ Assets/Scripts/Jesse/AnimationLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationLogic : MonoBehaviour
+{
+   public void Step()
+    {
+        PlaySound(Stepping, Stepping);
+    }
+
+    public void SwordSwing()
+    {
+        
+    }
+
+    public void block()
+    {
+
+    }
+
+    public void playerhit()
+    {
+
+    }
+
+}
