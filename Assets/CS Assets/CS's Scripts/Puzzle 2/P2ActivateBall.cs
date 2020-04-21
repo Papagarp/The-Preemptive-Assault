0f93bfcd6cd054bb71c786d73b7a744b6c0be505
CShepherdSAE <44623939+CShepherdSAE@users.noreply.github.com>
2020-04-21 15:46:25 +1000

    added some polish and doors

--- Assets/CS Assets/CS's Scripts/Puzzle 2/P2ActivateBall.cs
+++ Assets/CS Assets/CS's Scripts/Puzzle 2/P2ActivateBall.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class P2ActivateBall : MonoBehaviour
 {
+
+    public GameObject ballSpawner;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballSpawner.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            ballSpawner.SetActive(true);
+        }
     }
 }
---
+++ Assets/CS Assets/CS's Scripts/Puzzle 2/SpawnSphereScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSphereScript : MonoBehaviour
+{
+    public GameObject sphere;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "P2Floor")
+        {
+            Destroy(sphere);
+        }
+    }
+}
