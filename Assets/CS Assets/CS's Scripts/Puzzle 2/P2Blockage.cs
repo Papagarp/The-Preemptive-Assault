8d6fd94a82f61cecb037941e3e273d0783ed20f7
CShepherdSAE <44623939+CShepherdSAE@users.noreply.github.com>
2020-04-21 11:38:08 +1000

    worked on polish for puzzle 2

---
+++ Assets/CS Assets/CS's Scripts/Puzzle 2/P2Blockage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Blockage : MonoBehaviour
+{
+
+    public Animator anim;
+
+    private void Start()
+    {
+        anim.SetBool("BlockUp", false);
+    }
+
+    public void weightOn()
+    {
+        anim.SetBool("BlockUp", true);
+    }
+
+    public void weightOff()
+    {
+        anim.SetBool("BlockUp", false);
+    }
+}
---
+++ Assets/CS Assets/CS's Scripts/Puzzle 2/P2PressurePlate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2PressurePlate : MonoBehaviour
+{
+    public GameObject animatedObject;
+    P2Blockage animationController;
+
+    private void Start()
+    {
+        animationController = animatedObject.GetComponent<P2Blockage>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger Entered");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("If statment entered");
+            animationController.weightOn();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger Exited");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("If statment entered");
+            animationController.weightOff();
+        }
+    }
+}
