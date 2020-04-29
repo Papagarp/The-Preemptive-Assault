11288f17e954891654dab53413851d9531f6b70c
JesseNellis <40745615+JesseNellis@users.noreply.github.com>
2020-04-29 17:10:55 +1000

    boss music

--- Assets/Scripts/Ai/BossTrigger.cs
+++ Assets/Scripts/Ai/BossTrigger.cs
@@ -8,6 +8,8 @@ public class BossTrigger : MonoBehaviour
 
     GameObject cerberus;
 
+    JesseAudioManager jesseAudioManager;
+
     private void Awake()
     {
         cerberus = GameObject.FindGameObjectWithTag("Cerberus");
@@ -15,11 +17,18 @@ public class BossTrigger : MonoBehaviour
         cerberusScript = cerberus.GetComponent<CerberusController>();
     }
 
+    void Start()
+    {
+        jesseAudioManager = FindObjectOfType<JesseAudioManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             cerberusScript.startFight = true;
+            jesseAudioManager.StopSound("Music");
+            jesseAudioManager.PlaySound("Boss Music");
         }
     }
 }
